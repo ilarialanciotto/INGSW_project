907c90ff6b3724bcead21fcaae88dc257c7f522e
ilarialanciotto <ilarialan12@gmail.com>
2024-06-12 03:03:43 +0200

    modified class GraphicEvent

--- Project_Directory/src/GraphicObject/GraphicEvent.java
+++ Project_Directory/src/GraphicObject/GraphicEvent.java
@@ -3,14 +3,10 @@ package GraphicObject;
 
 public class GraphicEvent {
 
-
     private final GraphicObject source;
 
     public GraphicEvent(GraphicObject src) {
         source = src;
     }
 
-    public GraphicObject getSource() {
-        return source;
-    }
 }
